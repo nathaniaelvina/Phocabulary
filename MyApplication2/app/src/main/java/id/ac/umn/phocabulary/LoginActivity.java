59ba1f41c50445e600a9999db288046798d2974a
nathaniaelvina <nathaniaelvina@hotmail.com>
2017-04-13 19:30:04 +0700

    login n signin xml

---
+++ MyApplication2/app/src/main/java/id/ac/umn/phocabulary/LoginActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.phocabulary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
